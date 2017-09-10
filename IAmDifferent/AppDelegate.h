12ead02b985f4cb9b8b7d096ffdb468be6979534
JoshuaPiper <sqh1078534232@gmail.com>
2017-09-10 10:09:13 -0400

    Add files via upload

---
+++ IAmDifferent/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  I Am Different!
+//
+//  Created by Qihua Sun on Wednesday, January 13, 2016
+//  Copyright (c) Qihua Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
---
+++ IAmDifferent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  I Am Different!
+//
+//  Created by Qihua Sun on Wednesday, January 13, 2016
+//  Copyright (c) Qihua Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
